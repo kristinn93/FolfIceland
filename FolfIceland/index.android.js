32248a087d3fa054cade10ba345345b204d3a1fa
Arnar Þór Sveinsson <arnar.th92@gmail.com>
2016-08-13 16:02:00 +0000

    Adding relay to android as well

--- FolfIceland/index.android.js
+++ FolfIceland/index.android.js
@@ -1,27 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
+/* @flow */
 import React, {Component} from 'react'
 import {AppRegistry, StyleSheet, Text, View} from 'react-native'
-import setupRelay from './src/utils/relay'
+import {RootContainer} from 'react-relay'
 
+import CourseList from './src/components/Course'
+import CoursesRoute from './src/routes/Courses'
+
+import setupRelay from './src/utils/relay'
 
 class FolfIceland extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <RootContainer
+          Component={CourseList}
+          route={new CoursesRoute()}
+          renderFetched={(data) => <CourseList {...this.props} {...data} />}
+        />
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js
+          To get started, edit index.ios.js
         </Text>
         <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
         </Text>
       </View>
     )
