ea0f7f1d7686903217382d7e18e39e024819c27b
Arnar Þór Sveinsson <arnar.th92@gmail.com>
2016-08-21 17:54:49 +0000

    minor fixes for course service

--- server/services/courses.js
+++ server/services/courses.js
@@ -6,102 +6,202 @@ const courses: Map<string, Course> = fromJS({
   Garðalundur: {
     name: 'Garðalundur',
     city: 'Akranes',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Eiðsvöllur: {
     name: 'Eiðsvöllur',
     city: 'Akureyri',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Glerárskóli: {
     name: 'Glerárskóli',
     city: 'Akureyri',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Hamrakotstún: {
     name: 'Hamrakotstún',
     city: 'Akureyri',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Hamrar: {
     name: 'Hamrar',
     city: 'Akureyri',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Apavatn: {
     name: 'Apavatn',
     city: 'Laugarvatn',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Bifröst: {
     name: 'Bifröst',
     city: 'Bifröst',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Bolungarvík: {
     name: 'Bolungarvík',
     city: 'Bolungarvík',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Tjarnargarðurinn: {
     name: 'Tjarnargarðurinn',
     city: 'Egilsstaðir',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Flateyri: {
     name: 'Flateyri',
     city: 'Flateyri',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Flúðir: {
     name: 'Flúðir',
     city: 'Flúðir',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Víðistaðatún: {
     name: 'Víðistaðatún',
     city: 'Hafnafjörður',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Hrísey: {
     name: 'Hrísey',
     city: 'Hrísey',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Miðhúsaskógur: {
     name: 'Miðhúsaskógur',
     city: 'Laugarvatn',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Ævintýragarðurinn: {
     name: 'Ævintýragarðurinn',
     city: 'Mosfellsbær',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Neskaupsstaður: {
     name: 'Neskaupsstaður',
     city: 'Neskaupsstaður',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Rómantískasvæðið: {
     name: 'Rómantískasvæðið',
     city: 'Reykjanesbær',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   'Fella- og Hólahverfi': {
     name: 'Fella- og Hólahverfi',
     city: 'Breiðholt',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Fossvogsdalur: {
     name: 'Fossvogsdalur',
     city: 'Reykjavík',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Gufunes: {
     name: 'Gufunes',
     city: 'Grafarvogur',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Laugardalur: {
     name: 'Laugardalur',
     city: 'Reykjavík',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Seljahverfi: {
     name: 'Seljahverfi',
     city: 'Breiðholt',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Valhúsahæð: {
     name: 'Valhúsahæð',
     city: 'Seltjarnarnes',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Úlfljótsvatn: {
     name: 'Úlfljótsvatn',
     city: 'Úlfljótsvatn',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Vestmannaeyjar: {
     name: 'Vestmannaeyjar',
     city: 'Vestmannaeyjar',
+    location: {
+      lat: 64.138409,
+      long: -21.915763,
+    },
   },
   Klambratún: {
     name: 'Klambratún',
@@ -197,13 +297,10 @@ const courses: Map<string, Course> = fromJS({
 })
 
 export const getCourses = (): Array<{[key: string]: Course}> => {
-  const coursesList = courses.map((v: Map<string, any>, k) => {
-    return v.set('name', _.capitalize(k))
-  })
-  return coursesList.toList().sortBy((v) => v.get('city')).toJS()
+  return courses.toList().sortBy((v) => v.get('city')).toJS()
 }
 
 export const getCourse = (course: String): Course => {
-  return courses.get(course.toLowerCase()) &&
-    courses.get(course.toLowerCase()).set('name', _.capitalize(course)).toJS()
+  return courses.get(course) &&
+    courses.get(course).toJS()
 }
