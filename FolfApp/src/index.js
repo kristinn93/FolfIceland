373663b4c1eb9a968574ed5e7bef82ff94da62a1
Kristinn Júlíusson <kristinnj13@ru.is>
2017-05-07 01:10:41 +0000

    able to communicate with server

--- FolfApp/src/index.js
+++ FolfApp/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { comp as Root } from '../lib/js/src/root.js';
 
-export default class App extends React.Component {
+import { gql, graphql } from 'react-apollo';
+
+class App extends React.Component {
   render() {
+    console.log(this.props.data);
     return <Root message="Hello world" />;
   }
 }
+
+export default graphql(gql`
+  query {folf {courses {name}}}
+`)(App);
