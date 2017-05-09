85eeaa296cacdc813214aec23586c44f0ea89e8c
Arnar Þór Sveinsson <arnar.th92@gmail.com>
2017-05-09 23:21:47 +0000

    Restructuring course list and course details in Reason

---
+++ FolfApp/src/components/courseDetails.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import {gql, graphql} from 'react-apollo';
+import {
+  comp as CourseListRe,
+} from '../../lib/js/src/components/courseDetails.js';
+
+class CourseDetail extends React.Component {
+  static navigationOptions = {
+    title: 'Course Details',
+  };
+  render() {
+    return (
+      <CourseListRe
+        course={this.props.data && this.props.data.course}
+        loading={this.props.data.loading}
+      />
+    );
+  }
+}
+
+export default graphql(
+  gql`
+  query Courses($name: String!){
+  course(name: $name) {
+    name
+    city
+    location {
+      lat
+      long
+    }
+    numberOfBaskets
+    par {
+      red
+      white
+      blue
+    }
+  }
+}
+`,
+  {
+    options: ownProps => ({
+      variables: {
+        name: ownProps.navigation.state.params.name,
+      },
+    }),
+  }
+)(CourseDetail);
