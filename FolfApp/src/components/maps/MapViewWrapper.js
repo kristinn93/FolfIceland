f7c64c254be5dd9126a0c00f494cc73e08b1462e
Arnar Þór Sveinsson <arnar.th92@gmail.com>
2017-06-18 17:25:48 +0000

    Adding basket icon, not working yet

--- FolfApp/src/components/maps/MapViewWrapper.js
+++ FolfApp/src/components/maps/MapViewWrapper.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {MapView} from 'expo';
+import {Basket} from '../../icons/Basket';
 
 export const MapViewWrapper = props => {
+  console.log(Basket);
   return (
     <MapView style={props.style} region={props.region} showsUserLocation>
       {props.markers.map(marker =>
-        <MapView.Marker coordinate={marker} key={marker.latitude} />
+        <MapView.Marker coordinate={marker} key={marker.latitude}>
+          {/*{marker.basket === undefined ? <View><Basket /></View> : null}*/}
+        </MapView.Marker>
       )}
     </MapView>
   );
